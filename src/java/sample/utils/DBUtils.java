8d704fbc59d5eb3fa4d7e7c02665cb3f804e8bd0
BQViet1603 <vietbqhe181924@fpt.edu.vn>
2024-10-07 22:47:43 +0700

    Update DBUtils.java

--- src/java/sample/utils/DBUtils.java
+++ src/java/sample/utils/DBUtils.java
@@ -10,11 +10,12 @@ public class DBUtils {
         Connection cn = null;
         try {
             String url = "jdbc:sqlserver://localhost:1433;databaseName=PlantShop";
-            String username = "sa";
-            String password = "123";
+            String username = System.getenv("sa");
+            String password = System.getenv("123");
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             cn = DriverManager.getConnection(url, username, password);
         } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
         }
         return cn;
     }
